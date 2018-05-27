563e6e614fc867dc001f079c8042dfe4031d0a29
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-05-27 19:17:37 -0300

    first

--- test_db/index.js
+++ test_db/index.js
@@ -5,12 +5,27 @@
 //Não consegui fazer funcionar ainda com await, vejam os exemplos em
 // https://node-postgres.com/features/connecting
 
+const fs = require('fs');
 const { Client } = require('pg');
+
 const client = new Client();
 
-client.connect();
+async function clean_and_create_db () {
+    const create_script = fs.readFileSync('./criacao_bd.sql', 'utf8');
+    await client.query('DROP SCHEMA inline CASCADE');
+    await client.query(create_script);
+}
 
-client.query('SET search_path TO inline; SELECT * FROM usuario', (err, res) => {
-  console.log(err ? err.stack : res[1].rows[0]);
-  client.end();
-});
+async function insert_usuarios () {
+    await client.query("INSERT INTO inline.usuario VALUES ('Frank Silverio', 'cliente cadastrado', 'frank@uol.com', 'B28AJBBK28991SIOFG')");
+}
+
+async function connect_and_insert () {
+    await client.connect();
+    await clean_and_create_db();
+    await insert_usuarios();
+    client.end();
+}
+
+
+connect_and_insert();
