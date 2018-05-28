adfaaba785d5b252e33e71b3b5b40f093a4a0448
Lucas Oshiro <lucasseikioshiro@gmail.com>
2018-05-27 21:02:27 -0300

    insert, remove

--- test_db/index.js
+++ test_db/index.js
@@ -17,46 +17,36 @@ async function clean_and_create_db () {
 }
 
 async function insert_clientes_cadastrados () {
-    await client.query("INSERT INTO inline.usuario
-                        VALUES ('Frank Silverio', 'cliente cadastrado', 'frank@uol.com', 'B28AJBBK28991SIOFG'),
-                               ('Emerson Lake Palmer', 'cliente cadastrado', 'emerson@elp.com', 'B28AJBBK28991SIOFG')");
-
-    // await client.query("INSERT INTO inline.cliente_cadastrado
-    //                     VALUES ('frank@uol.com', 1),
-    //                            (emerson@elp.com, 2)");
+    await client.query("INSERT INTO inline.usuario VALUES ('Frank Silverio', 'cliente cadastrado', 'frank@uol.com', 'B28AJBBK28991SIOFG'), ('Emerson Lake Palmer', 'cliente cadastrado', 'emerson@elp.com', 'B28AJBBK28991SIOFG')");
+    await client.query("INSERT INTO inline.cliente VALUES (1, '987654321', '-'), (2, '987654322', '-')")
+    await client.query("INSERT INTO inline.cliente_cadastrado VALUES ('frank@uol.com', 1), ('emerson@elp.com', 2)");
 }
 
-// async function insert_empresas () {
-//     await client.query("INSERT INTO inline.usuario
-//                         VALUES ('Fanfa Bar & Snacks', 'empresa', 'fanfa@fanfa.com', 'B28AJBBK28991SIOFG')");
+async function insert_empresas_estabelecimentos () {
+    await client.query("INSERT INTO inline.usuario VALUES ('Fanfa Bar & Snacks', 'empresa', 'fanfa@fanfa.com', 'B28AJBBK28991SIOFG'), ('Fanfa Faria Lima', 'estabelecimento', 'fanfafa@fanfa.com', 'B28AJBBK28991SIOFG')");
+    await client.query("INSERT INTO endereco VALUES (1, 'SP', 'São Paulo', 'Rua do Matagal', 10, 'B5'), (2, 'SP', 'São Paulo', 'Av. Faria Lima', 10, 'A113')");
+    await client.query("INSERT INTO inline.empresa VALUES ('fanfa@fanfa.com', '234152251', 1)");
+    await client.query("INSERT INTO inline.estabelecimento VALUES ('fanfafa@fanfa.com', 'fanfa@fanfa.com', 2, '0S0E')");
+}
 
-//     await client.query("INSERT INTO endereco
-//                         VALUES (1, 'SP', 'São Paulo', 'Rua do Matagal', 10, 'B5')");
+async function insert_avaliacao () {
+    await client.query("INSERT INTO inline.avaliacao VALUES (1, 4, 'Muito bom, mas pode ser melhor', 'fanfafa@fanfa.com', 'emerson@elp.com')");
+    await client.query("INSERT INTO inline.avaliacao VALUES (2, 5, 'Excelente', 'fanfafa@fanfa.com', 'frank@uol.com')");
+}
 
-//     await client.query("INSERT INTO inline.empresa
-//                         VALUES ('fanfa@fanfa.com', '234152251', 1)");
-// }
+async function remove_avaliacao () {
+    await client.query("DELETE FROM inline.avaliacao WHERE id = 2");
+}
 
 async function connect_and_insert () {
     await client.connect();
     await clean_and_create_db();
     await insert_clientes_cadastrados();
-    await inser_empresas();
+    await insert_empresas_estabelecimentos();
+    await insert_avaliacao();
+    await remove_avaliacao();
     client.end();
 }
 
-async function selectquery (sql) {
-    await client.connect();
-    const res = await client.query(sql);
-    client.end();
-    return res.rows;
-}
 
 connect_and_insert();
-
-console.log(selectquery("SELECT * FROM inline.avaliacao"));
-console.log(selectquery("SELECT * FROM inline.cliente WHERE tipo='cliente cadastrado'"));
-console.log(selectquery("SELECT * FROM inline.usuario"));
-selectquery('SELECT * 
-             FROM inline.cliente INNER JOIN inline.cliente_cadastrado ON
-                cliente.id = cliente_cadastrado.id_cliente');
